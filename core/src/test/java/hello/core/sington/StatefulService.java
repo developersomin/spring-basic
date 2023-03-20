2b865468bac0b618597f33229065a41548f7c92b
somin <angiwon12345@nate.com>
2023-03-20 18:18:05 +0900

    #5 - 싱글톤

---
+++ core/src/test/java/hello/core/sington/StatefulService.java
@@ -0,0 +1,8 @@
+package hello.core.sington;
+
+public class StatefulService {
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        return price;
+    }
+}
